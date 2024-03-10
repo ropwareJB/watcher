42c390ecbfad5bb7ba562bc311f395fbe63cbd51
Josh Brown <josh@epidev.com>
2024-03-10 17:36:07 +1100

    render nodes

--- client/src/Page/Tree.elm
+++ client/src/Page/Tree.elm
@@ -2,14 +2,32 @@ module Page.Tree exposing (..)
 
 import Html exposing (..)
 import Material
+import Material.Options as Options exposing (cs, css, id, styled, when)
 
 
 type alias Lift m =
     Msg m -> m
 
 
+type Node
+    = Node { children : List Node }
+
+
+root : Node
+root =
+    Node
+        { children =
+            [ Node { children = [] }
+            , Node { children = [] }
+            , Node { children = [] }
+            ]
+        }
+
+
 type alias Model m =
-    { mdc : Material.Model m }
+    { mdc : Material.Model m
+    , root : Node
+    }
 
 
 type Msg m
@@ -19,7 +37,9 @@ type Msg m
 
 init : Lift m -> ( Model m, Cmd m )
 init lift =
-    ( { mdc = Material.defaultModel }
+    ( { mdc = Material.defaultModel
+      , root = root
+      }
     , Cmd.none
     )
 
@@ -33,4 +53,16 @@ view : Model m -> Html m
 view model =
     Html.div
         []
-        [ text "yeobuseyo" ]
+        [ viewNode model.root ]
+
+
+viewNode : Node -> Html m
+viewNode (Node node) =
+    styled Html.div
+        [ css "display" "inline-block" ]
+    <|
+        [ styled Html.div
+            [ cs "node" ]
+            []
+        ]
+            ++ List.map viewNode node.children
